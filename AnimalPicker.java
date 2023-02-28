3c6a5ddfded70f5612036b8b4a9e5c9d1e164efd
atoomer <atoomer@quinnipiac.com>
2023-02-28 14:07:17 -0500

    Accepts cat or dog and prints placeholder for ASCII art.

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class AnimalPicker {
+
+    public static String animal;
+    public static String cat = "cat";
+    public static String dog = "dog";
+
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What would you like to see: a cat or a dog? ");
+        String animal = scan.next();
     
+        if (animal.equals(cat)){
+            System.out.println("CAT ASCII ART HERE");
+        }
+        else if(animal.equals(dog)){
+            System.out.println("DOG ASCII ART HERE");
+        }
+        else{
+            System.out.println("huh");
+        }
+        
+        
+    }
+   
+    
+
 }
